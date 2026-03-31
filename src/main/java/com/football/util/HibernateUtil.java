50ed7c0342ba0e9282f42310c3c0f3e7f52b02da
Quân <anhquan2006py2@gmail.com>
2026-03-31 12:40:03 +0700

    Thêm hibernateUlti

--- src/main/java/com/football/util/HibernateUtil.java
+++ src/main/java/com/football/util/HibernateUtil.java
@@ -1,5 +1,27 @@
 package com.football.util;
 
-public class HibernateUtil {
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Tạo SessionFactory từ file hibernate.cfg.xml
+            return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println(">>> Lỗi khởi tạo SessionFactory: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        // Đóng caches và connection pools 
+        getSessionFactory().close();
+    }
 }
